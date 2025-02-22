b5131a9ed8fd964587ddce9c15a99f884001ff5f
Teletrup <saletrapotasowatosyf@gmail.com>
2025-02-22 05:53:48 +0100

    fixed xxd, basic typedefs

--- main.c
+++ main.c
@@ -1,31 +1,40 @@
 int putchar(char c);
 int getchar();
 
+typedef signed char I1;
+typedef unsigned char U1;
+typedef signed short I2;
+typedef unsigned short U2;
+typedef signed int I4;
+typedef unsigned int U4;
+typedef signed long I8;
+typedef unsigned long U8;
 
-char heap[0xFFFF];
+
+U1 heap[0xFFFF];
 
 
-void pc(char c) {
+void pc(U1 c) {
   putchar(c);
 }
 
-int gc() {
+U4 gc() {
   getchar();
 }
 
-void ps(char* s) {
+void ps(U1* s) {
   for (;*s!=0;s++) {
     pc(*s);
   }
 }
 
-void pbl (int x) {
+void pbl (U4 x) {
   pc(x ? '1' : '0');
 }
 
-void pb(char n) {
-  for (int i=8; i>=0; i--) {
-    char a = n >> i;
+void pb(U1 n) {
+  for (U4 i=8; i>=0; i--) {
+    U1 a = n >> i;
     pbl(a);
     n -= a << i;
   }
@@ -35,59 +44,59 @@ void pn() {
   pc('\n');
 }
 
-void pB(char n) {
-  char dg[] = "0123456789ABCDEF";
-  char hi4 = n >> 4;
-  char lo4 = n - (hi4 << 4);
+void pB(U1 n) {
+  U1 dg[] = "0123456789ABCDEF";
+  U1 hi4 = n >> 4;
+  U1 lo4 = n - (hi4 << 4);
   pc(dg[hi4]);
   pc(dg[lo4]);
 }
 
-void pnB(char* buf, int n) {
-  for (int i=0; i<n; i++) {
+void pnB(U1* buf, U4 n) {
+  for (U4 i=0; i<n; i++) {
     pB(buf[i]);
   }
 }
 
-void pX(int x) {
-  char* dg = "0123456789ABCDEF";
-  int d = x / 16;
-  int r = x % 16;
+void pX(U4 x) {
+  U1* dg = "0123456789ABCDEF";
+  U4 d = x / 16;
+  U4 r = x % 16;
   if (d) pX(d);
   pc(dg[r]);
 }
 
-void pd(int x) {
-  char* dg = "0123456789";
-  int d = x / 10;
-  int r = x % 10;
+void pd(U4 x) {
+  U1* dg = "0123456789";
+  U4 d = x / 10;
+  U4 r = x % 10;
   if (d) pd(d);
   pc(dg[r]);
 }
 
 
-int max(int a, int b) {
+U4 max(U4 a, U4 b) {
   return a > b ? a : b;
 }
 
-int min(int a, int b) {
+U4 min(U4 a, U4 b) {
   return a < b ? a : b;
 }
 
-void xxd(char* buf, int len) {
-  int sz = 16;
-  int line_n = len / sz + !!(len % sz);
-  for (int i=0; i<line_n; i++) {
-    int off = sz * i;
-    int byte_n = min(len - off, sz);
-    for (int j=0; j<byte_n; j++) {
+void xxd(U1* buf, U4 len) {
+  U4 sz = 16;
+  U4 line_n = len / sz + !!(len % sz);
+  for (U4 i=0; i<line_n; i++) {
+    U4 off = sz * i;
+    U4 byte_n = min(len - off, sz);
+    for (U4 j=0; j<byte_n; j++) {
       pB(buf[off + j]);
       if (j % 2 == 1) pc(' ');
     }
-    int pad_n = sz/2*5 - (byte_n * 2) - (byte_n / 2) + 1;
-    for (int j=0; j<pad_n; j++) pc(' ');
-    for (int j=0; j<byte_n; j++) {
-      char c = buf[off + j];
+    U4 pad_n = sz/2*5 - (byte_n * 2) - (byte_n / 2) + 1;
+    for (U4 j=0; j<pad_n; j++) pc(' ');
+    for (U4 j=0; j<byte_n; j++) {
+      U1 c = buf[off + j];
       if (c >= ' ' && c <= '~') pc(c);
       else pc('.');
     }
@@ -95,25 +104,24 @@ void xxd(char* buf, int len) {
   }
 }
 
-int slen(char* s) {
-  int i = 0;
+U4 slen(U1* s) {
+  U4 i = 0;
   for (; s[i]; i++);
   return i;
 }
 
 void megadump() {
   //maximum range to not segfault
-  pnB("aaaa"-8*1024-4, 84*1024);
+  xxd("aaaa"-8*1024-4, 82*1024);
 }
 
-int main() {
-  //megadump();
-  //pX(128);
-  //pX(84*1024 - (8*1024+4));
-  //char* foo = "sdfghjklasa;dsfghjdkksjkfsdlfkdsdjfdskfklsdfsdjlf";
-  char foo[] = "1234567890abcdefghi";
+U4 main() {
+  megadump();
+  /*
+  U1 foo[] = "1234567890abcdefghi";
   foo[10] = 10;
   xxd(foo, slen(foo));
+  */
   pd(1337);
   pn();
 }
